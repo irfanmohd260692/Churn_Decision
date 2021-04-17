84c3a6e3551b5588c747b31307eeb1ee042a3fd4
irfanmohd260692 <81827458+irfanmohd260692@users.noreply.github.com>
2021-04-17 16:17:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ def predict():
 	final_features = [np.array(int_features)]
 	prediction = model.predict(final_features)
 	if prediction ==0:
-		return render_template('index.html',prediction_text='Customer is Good'.format(prediction),
+		return render_template('index.html',prediction_text="Customer is Good".format(prediction),
                                )
 	else:
 		return render_template('index.html',
-                               prediction_text='Customer is Bad'.format(prediction),
+                               prediction_text="Customer is Bad".format(prediction),
                               )
 	
 if __name__ == "__main__":
